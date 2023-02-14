8569f955c24937a5fe2fd17e77ee58095c2ab9b4
Rafael Huaman <leafar.huaman@gmail.com>
2023-02-13 23:05:12 -0500

    Set up jest with Chakra UI Provider

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,1 +1,7 @@
+import { queryClient } from "@/test-utils";
 import "@testing-library/jest-dom";
+
+beforeEach(() => {
+  // Clear TanStack Query cache
+  queryClient.clear();
+});
---
+++ src/test-utils.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>{children}</ChakraProvider>
+    </QueryClientProvider>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
