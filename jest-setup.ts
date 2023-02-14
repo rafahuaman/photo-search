07accc819e8a40ea6083ee2209a622108d14aa20
Rafael Huaman <leafar.huaman@gmail.com>
2023-02-14 05:52:36 -0500

    set up mock-match-media

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,7 +1,12 @@
-import { queryClient } from "@/test-utils";
+import { queryClient, setMobileScreen } from "@/test-utils";
 import "@testing-library/jest-dom";
+import mockMatchMedia from "mock-match-media";
 
 beforeEach(() => {
   // Clear TanStack Query cache
   queryClient.clear();
+
+  // Mobile-first: all tests are run on a mobile screen by default
+  mockMatchMedia.cleanup();
+  setMobileScreen();
 });
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
+import { setMedia } from "mock-match-media/dist";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,5 +29,25 @@ const customRender = (
   options?: Omit<RenderOptions, "wrapper">
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
+// re-export everything from testing-library
 export * from "@testing-library/react";
+export * as userEvent from "@testing-library/user-event";
+// override render method
 export { customRender as render };
+
+export const setMobileScreen = () => {
+  // Based on Iphone 7 screen size
+  setMedia({
+    width: "375px",
+    type: "screen",
+    orientation: "portrait",
+  });
+};
+
+export const setDesktopScreen = () => {
+  setMedia({
+    width: "993px",
+    type: "screen",
+    orientation: "portrait",
+  });
+};
