ab1de3100f704a61492952709ff8bd1893731799
Rafael Huaman <leafar.huaman@gmail.com>
2023-02-15 07:29:44 -0500

    Add skip to content

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { Flex } from "@chakra-ui/react";
+import { SkipNavContent, SkipNavLink } from "@chakra-ui/skip-nav";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Main from "./Main";
@@ -7,8 +8,12 @@ import NavBar from "./NavBar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <Flex direction="column" flex="1">
+      <SkipNavLink>Skip to content</SkipNavLink>
       <NavBar />
-      <Main>{children}</Main>
+      <Main>
+        <SkipNavContent />
+        {children}
+      </Main>
       <Footer />
     </Flex>
   );
