8733de91c67bb45a014892daac1b9984b1c70ea7
Rafael Huaman <leafar.huaman@gmail.com>
2023-03-19 21:25:28 -0400

    Update Masonry Layout to use Flex columns to ensure priority of first images

--- src/components/MasonryPhotoLayout.tsx
+++ src/components/MasonryPhotoLayout.tsx
@@ -1,14 +1,14 @@
 import { Photo } from "@/types";
-import { Box, Fade } from "@chakra-ui/react";
+import { Fade, HStack, VStack } from "@chakra-ui/react";
 import PhotoCard from "./PhotoCard";
 
-type MasonryLayoutProps = {
+type MasonryColumnProps = {
   photos: Photo[];
 };
 
-function MasonryPhotoLayout({ photos }: MasonryLayoutProps) {
+function MasonryColumn({ photos }: MasonryColumnProps) {
   return (
-    <Box sx={{ columnCount: [1, 2, 3], columnGap: 8, columnFill: "balance" }}>
+    <VStack spacing={8}>
       {photos.map(
         (
           {
@@ -23,24 +23,59 @@ function MasonryPhotoLayout({ photos }: MasonryLayoutProps) {
           },
           index
         ) => (
-          <Box key={id} mb={8}>
-            <Fade in>
-              <PhotoCard
-                id={id}
-                url={url}
-                photographerName={photographerName}
-                photographerUrl={photographerUrl}
-                alt={alt}
-                priority={index < 3}
-                originalWidth={width}
-                originalHeight={height}
-                placeholderColor={placeholderColor}
-              />
-            </Fade>
-          </Box>
+          <Fade key={id} in>
+            <PhotoCard
+              id={id}
+              url={url}
+              photographerName={photographerName}
+              photographerUrl={photographerUrl}
+              alt={alt}
+              priority={index === 0}
+              originalWidth={width}
+              originalHeight={height}
+              placeholderColor={placeholderColor}
+            />
+          </Fade>
         )
       )}
-    </Box>
+    </VStack>
+  );
+}
+
+function buildThreeColumnsInHorizontalOrder(photos: Photo[]) {
+  const column1 = [];
+  const column2 = [];
+  const column3 = [];
+  for (let i = 0; i < photos.length; i++) {
+    const remainder = (i + 1) % 3;
+    switch (remainder) {
+      case 1:
+        column1.push(photos[i]);
+        break;
+      case 2:
+        column2.push(photos[i]);
+        break;
+      case 0:
+        column3.push(photos[i]);
+        break;
+    }
+  }
+  return [column1, column2, column3];
+}
+
+type MasonryLayoutProps = {
+  photos: Photo[];
+};
+
+function MasonryPhotoLayout({ photos }: MasonryLayoutProps) {
+  const [column1, column2, column3] =
+    buildThreeColumnsInHorizontalOrder(photos);
+  return (
+    <HStack wrap={"wrap"} spacing={8} align="start">
+      <MasonryColumn photos={column1} />
+      <MasonryColumn photos={column2} />
+      <MasonryColumn photos={column3} />
+    </HStack>
   );
 }
 
